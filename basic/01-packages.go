7387cb6b8110c2f612d862aba47fa6b6480e77e7
raryosu <me@raryosu.info>
2018-12-08 18:20:01 +0900

    Add basic :tada:

---
+++ basic/01-packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite numver is", rand.Intn(10))
+}
---
+++ basic/14-type-interface.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := "hello"
+	fmt.Printf("v is of type %T\n", v)
+}
